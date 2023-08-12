8cd849f4648ee65047df1870e3efa914ab2e063e
Sushil Gupta <kalwarsushil2@gmail.com>
2023-08-12 14:07:31 +0545

    Created a servlet

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -1,6 +1,8 @@
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HelloServlet
  */
-@WebServlet("/HelloServlet")
+@WebServlet(name = "HelloServlet", urlPatterns = "/Hello")
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -27,7 +29,12 @@ public class HelloServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h1>");
+		out.println("Hello from Srvelet");
+		out.println("</h1>");
+		System.out.print("Hello from Servlet");
 	}
 
 	/**
